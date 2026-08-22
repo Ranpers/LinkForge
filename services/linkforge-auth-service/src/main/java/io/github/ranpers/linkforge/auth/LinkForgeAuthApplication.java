ab2a795475a8e0fbb6d48d965ce6f524cf15d260
Yiran <yi_ran@aliyun.com>
2026-08-22 10:12:58 +0800

    feat: bootstrap OAuth2 authorization server

---
+++ services/linkforge-auth-service/src/main/java/io/github/ranpers/linkforge/auth/LinkForgeAuthApplication.java
@@ -0,0 +1,11 @@
+package io.github.ranpers.linkforge.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkForgeAuthApplication {
+    static void main(String[] args) {
+        SpringApplication.run(LinkForgeAuthApplication.class, args);
+    }
+}
